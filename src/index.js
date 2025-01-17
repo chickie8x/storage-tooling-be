c1c4a43268b949171ed3d4be4ec03b8d2582f1f4
chickie8x <chickie8x.qn@gmail.com>
2025-01-17 09:29:10 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -8,14 +8,7 @@ import { generateToken, verifyToken, authMiddleware } from './utils/index.js';
 const app = express();
 app.use(express.json());  
 app.use(express.urlencoded())
-
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Add allowed origins
-  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 //create user
