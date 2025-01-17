c6e3dd4f21b38425cd588af8dcf12a2af7794ae9
chickie8x <chickie8x.qn@gmail.com>
2025-01-17 09:23:01 +0700

    add cors option

--- src/index.js
+++ src/index.js
@@ -8,18 +8,14 @@ import { generateToken, verifyToken, authMiddleware } from './utils/index.js';
 const app = express();
 app.use(express.json());  
 app.use(express.urlencoded())
-app.use(cors());
 
-// app.get('/', async (req, res) => {
-//   res.json({message: 'Hello World'});  
-// });
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Add allowed origins
+  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+};
 
-// app.get('/users/:id', async (req, res) => {
-//   const user = await prisma.user.findUnique({
-//     where: { id: parseInt(req.params.id) },
-//   });
-//   res.json(user);
-// });
+app.use(cors(corsOptions));
 
 
 //create user
