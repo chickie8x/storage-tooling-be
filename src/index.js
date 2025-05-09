ca85a8efa01e0ee442599d9ea0ce85c6e9bddfe6
chickie8x <chickie8x.qn@gmail.com>
2025-05-09 22:31:10 +0700

    increase limit request payload

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { generateToken, verifyToken, authMiddleware } from './utils/index.js';
 
 
 const app = express();
-app.use(express.json());  
-app.use(express.urlencoded())
+app.use(express.json({limit: '5mb'}));  
+app.use(express.urlencoded({limit: '5mb', extended: true}))
 app.use(cors());
 
 
